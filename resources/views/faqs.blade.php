d2d00f8c2bd7a4daea92f402c7419532796097c7
mldp-dev <mariellaliezlp@gmail.com>
2023-12-14 11:40:55 +0800

    updated frequently asked questions file

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -11,6 +11,26 @@
                 <div class="p-6 text-gray-900">
                     {{ __("Frequently Asked Questions!") }}
                 </div>
+                <div class="p-6 text-gray-900">
+                    {{ __("1. Can I view my attendance history? Yes, you should be able to access your attendance history, which includes details like dates, times, and locations.") }}
+                </div>
+                <div class="p-6 text-gray-900">
+                    {{ __("2. What should I do if I forget to punch in or out?
+The system allows manual entry, or inform your supervisor or the system administrator to make necessary corrections.") }}
+                </div>
+                <div class="p-6 text-gray-900">
+                    {{ __("3. Is the system accessible for remote work or flexible schedules?
+Yes, the systems accommodate remote work by allowing employees to log attendance from various locations.
+") }}
+                </div>
+                <div class="p-6 text-gray-900">
+                    {{ __("4. How do I add my accomplishments to the system?
+You can typically add your accomplishments through an interface provided by the system. Look for an Add Accomplishment feature.") }}
+                </div>
+                <div class="p-6 text-gray-900">
+                    {{ __("5. Can I access the Accomplishment System from outside the office?
+Yes, you might be able to access the Accomplishment System remotely.") }}
+                </div>
             </div>
         </div>
     </div>
